f6372d385608411003f4f86077e62d592ede35fd
Sayujya Bastola <124507889+saayuj-19@users.noreply.github.com>
2025-07-04 15:34:04 +0000

    Email Verification Implementation

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_mail import Mail, Message
 import random
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
-app.secret_key = 'your_super_secret_key_here'
+app.secret_key = os.getenv('SECRET_KEY')
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'noreply.info.testgenie@gmail.com'
-app.config['MAIL_PASSWORD'] = 'kmhc nwfj goea xrrj'
-app.config['MAIL_DEFAULT_SENDER'] = 'noreply.info.testgenie@gmail.com'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
 mail = Mail(app)
 
